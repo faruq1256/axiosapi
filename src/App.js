a6ee3bf19d847c886f1ef4febe98ea2f1cea8f0d
Faruq Shaik <faruq1256@gmail.com>
2020-11-30 15:33:22 +0530

    axios API Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(1);
+  const [title, setTitle] = useState();
+  const [move, setMove] = useState();
+
+  useEffect(() => {
+    //  alert("Completed");
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+      const title = res.data.name;
+      setTitle(title);
+      const move = res.data.moves.length;
+      setMove(move);
+      console.log(res);
+    }
+    getData();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2> You have selected {num} </h2>
+      <p> Pokeman Name is {title}</p>
+      <p>Number of Moves {move}</p>
+      <select
+        onChange={() => {
+          setNum(num + 1);
+        }}
+      >
+        <option value="1">1</option>
+        <option value="3">3</option>
+        <option value="5">5</option>
+        <option value="25">25</option>
+        <option value="9">9</option>
+      </select>
+    </>
   );
-}
+};
 
 export default App;
